965d897da6ca1accda42a73031c8ae5345daa5f3
David Carmichael <carmichael.db88@gmail.com>
2023-10-18 17:31:42 +0000

    create client.js and move connect function to client.js

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.on('connect', () => {
+    console.log("Lets play Snek!");
+  });
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+
+module.exports = {
+  connect: connect };
--- play.js
+++ play.js
@@ -1,24 +1,3 @@
-const net = require("net");
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.on('connect', () => {
-    console.log("Ready to play Snek?");
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+connect.connect();
