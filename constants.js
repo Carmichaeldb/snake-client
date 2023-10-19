614b8712f9e4973b601934dffde49d5366e88ab0
David Carmichael <carmichael.db88@gmail.com>
2023-10-19 23:36:37 +0000

    make snek move continously with setInterval

--- constants.js
+++ constants.js
@@ -1,19 +1,22 @@
 const IP = 'localhost';
 const PORT = 50541;
 const stdin = process.stdin;
-const INPUT = {
-  '1': "Say: Watch out!",
-  '2': "Say: I'll get you!",
-  '3': "Say: Nice Try!",
+const INPUT_MOVEMENT = {
   'w': "Move: up",
   'a': "Move: left",
   's': "Move: down",
   'd': "Move: right",
+};
+const INPUT_MESSAGE = {
+  '1': "Say: Watch out!",
+  '2': "Say: I'll get you!",
+  '3': "Say: Nice Try!",
 };
 
 module.exports = {
   IP: IP,
   PORT: PORT,
   stdin: stdin,
-  INPUT: INPUT
+  INPUT_MOVEMENT: INPUT_MOVEMENT,
+  INPUT_MESSAGE: INPUT_MESSAGE
 };
--- input.js
+++ input.js
@@ -2,7 +2,7 @@
 // "Move: down" - move down one square (unless facing up)
 // "Move: left" - move left one square (unless facing right)
 // "Move: right" - move left one square (unless facing left)
-const {stdin, INPUT} = require("./constants");
+const {stdin, INPUT_MOVEMENT, INPUT_MESSAGE} = require("./constants");
 let connection;
 
 const setupInput = function (conn) {
@@ -13,34 +13,26 @@ const setupInput = function (conn) {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
+let intervalMovement;
+
 const handleUserInput = function (data) {
   if (data === '\u0003') { //looks if ctrl c is pressed
     process.exit(); //ends connection
   }
-  if (INPUT[data]) {
-    connection.write(INPUT[data]);
+
+  if (INPUT_MOVEMENT[data]) {
+    if (intervalMovement) {
+      clearInterval(intervalMovement);
+    }
+    intervalMovement = setInterval(() => {
+      connection.write(INPUT_MOVEMENT[data]);
+    }, 50);
+    intervalMovement;
   }
-  // if (data === 'w') {
-  //   connection.write("Move: up");
-  // }
-  // if (data === 'a') {
-  //   connection.write("Move: left");
-  // }
-  // if (data === 's') {
-  //   connection.write("Move: down");
-  // }
-  // if (data === 'd') {
-  //   connection.write("Move: right");
-  // }
-  // if (data === '1') {
-  //   connection.write("Say: Watch out!");
-  // }
-  // if (data === '2') {
-  //   connection.write("Say: I'll get you!");
-  // }
-  // if (data === '3') {
-  //   connection.write("Say: Nice Try!");
-  // }
+  if (INPUT_MESSAGE[data]) {
+    connection.write(INPUT_MESSAGE[data]);
+  }
 };
 
 module.exports = {
